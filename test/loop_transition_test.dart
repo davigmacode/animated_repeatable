d5990c4801822aaa0722d80f9761cc7c6e1b287a
Irfan Vigma Taufik <davigmacode@gmail.com>
2024-04-26 01:15:09 +0700

    Dummy test

--- test/loop_transition_test.dart
+++ test/loop_transition_test.dart
@@ -1,6 +1,6 @@
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test/flutter_test.dart';
 // import 'package:loop_transition/loop_transition.dart';
 
 void main() {
-  // test('adds one to input values', () {});
+  test('adds one to input values', () {});
 }
